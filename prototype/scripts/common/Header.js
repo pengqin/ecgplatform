4e08371172937b5b1d5be507c8ab688cf1940dd0
hopesfish <hopesfish@163.com>
2013-06-14 04:21:35 -0400

    优化模板结构，基于DRY原则

--- prototype/scripts/common/Header.js
+++ prototype/scripts/common/Header.js
@@ -1,9 +1,15 @@
 'use strict';
 define(function(require, exports) {
   var headerTemplate = require("./templates/header.html");
+  var subheaderTemplate = require("./templates/subheader.html");
 
   angular.module('ecgHeader', [])
   .controller('HeaderController', function ($scope) {
+  })
+  .controller('SubHeaderController', function ($scope) {
+      $scope.subheader = {
+          title: "请选择菜单"
+      };
   })
   .directive("ecgHeader", ['$location', function ($location) {
     return {
@@ -11,6 +17,16 @@ define(function(require, exports) {
       replace: false,
       template: headerTemplate,
       controller: "HeaderController",
+      link: function ($scope, $element, $attrs) {
+      }
+    };
+  }])
+  .directive("ecgSubHeader", ['$location', function ($location) {
+    return {
+      restrict: 'E',
+      replace: false,
+      template: subheaderTemplate,
+      controller: "SubHeaderController",
       link: function ($scope, $element, $attrs) {
       }
     };
--- prototype/scripts/task/main.js
+++ prototype/scripts/task/main.js
@@ -8,9 +8,11 @@ angular.module('ecgTask', ['ecgTaskService', 'ecgUndoneTask'])
 .controller('UndoneTaskController', ['$scope', function ($scope) {
     // register the inner namespace
     $scope.undone = {};
+    $scope.subheader.title = "待办工作";
 }])
 .controller('DoneTaskController', ['$scope', function ($scope) {
     // register the inner namespace
     $scope.done = {};
+    $scope.subheader.title = "已办工作";
 }]);
 });
