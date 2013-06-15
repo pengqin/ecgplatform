6ab0a3f0094ef46330b1101ca4477a6c8f26c689
hopesfish <hopesfish@163.com>
2013-06-16 00:16:02 +0800

    修改plot的zoom范围

--- prototype/scripts/task/directives/Plot.js
+++ prototype/scripts/task/directives/Plot.js
@@ -23,7 +23,7 @@ define(function(require, exports) {
                         enable: true,
                         levelRange: {
                             lower : 5,
-                            upper : 10
+                            upper : 8
                         }
                     }
                 }
