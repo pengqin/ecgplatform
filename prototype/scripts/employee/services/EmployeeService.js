f0d25220ae879bc7ca4abcf1c151075221bba5fe
hopesfish <hopesfish@163.com>
2013-06-18 23:50:52 +0800

    删除多余目录

--- prototype/scripts/employee/services/EmployeeService.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-define(function(require, exports) {
-
-angular.module('ecgEmployeeService', [])
-    .factory("EmployeeService", function() {
-        return {
-        }
-    });
-});
