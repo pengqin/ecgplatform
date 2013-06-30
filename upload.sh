d83ed6117fe7256ffc64e90df8a9fd757e09dc55
hopesfish <hopesfish@163.com>
2013-06-30 07:56:56 -0400

    构建0756

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ ftp -v -n 182.18.31.105 << EOF
 user ainia ppainia
 binary
 cd /opt/jetty7/webapps
-lcd web/target
+lcd /home/travis/build/hopesfish/ecgplatform/web/target
 prompt
 put web.war web.war
 close
