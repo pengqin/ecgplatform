3abba4ff04ea549a8b52308585cb1c2b6fddac2f
hopesfish <hopesfish@163.com>
2013-07-18 00:24:41 +0800

    修改标题

--- web/src/main/webapp/scripts/ie.js
+++ web/src/main/webapp/scripts/ie.js
@@ -10,7 +10,4 @@ document.createElement('at-pagination');
 /* ecg custom element definition */
 // monitor module
 document.createElement('ecg-employee-status-report');
-document.createElement('ecg-experts-task-report');
-document.createElement('ecg-experts-task-report');
-document.createElement('ecg-operators-task-report');
 document.createElement('ecg-task-process-report');
