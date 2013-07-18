991f3efff00d6656d6e03b765d61a3a5db8b78e8
hopesfish <hopesfish@163.com>
2013-07-18 13:41:41 +0800

    1 增加工作状态修改 2 晚上监控界面

--- web/src/main/webapp/scripts/common/Header.js
+++ web/src/main/webapp/scripts/common/Header.js
@@ -1,38 +1,78 @@
 'use strict';
 define(function(require, exports) {
-  var headerTemplate = require("./templates/header.html");
-  var subheaderTemplate = require("./templates/subheader.html");
+var headerTemplate = require("./templates/header.html");
+var subheaderTemplate = require("./templates/subheader.html");
 
-  angular.module('ecgHeader', [])
-  .controller('HeaderController', function ($scope) {
+angular.module('ecgHeader', [])
+.controller('HeaderController', ['$scope', 'EnumService', 'ProfileService', function ($scope, EnumService, ProfileService) {
     $scope.logout = function() {
-      window.logout();
+        window.logout();
     };
-  })
-  .controller('SubHeaderController', function ($scope) {
-      $scope.subheader = {
-          title: "请选择菜单"
-      };
-  })
-  .directive("ecgHeader", ['$location', function ($location) {
+
+    $scope.header = {};
+    $scope.header.status = null;
+    $scope.header.statuses = EnumService.getEmployeeStatus();
+    $scope.header.getEmployeeStatusLabel = EnumService.getEmployeeStatusLabel;
+
+    $scope.$watch("session.user", function() {
+        if (!$scope.session.user.id) { return; }
+        $scope.header.status = $scope.session.user.status;
+        if ($scope.header.status === 'OFFLINE') {
+            $scope.dialog.confirm({
+                text: "您是否要切换到正在工作的状态!",
+                handler: function() {
+                    $scope.header.setStatus('ONLINE');
+                }
+            });
+        }
+    });
+
+    $scope.header.setStatus = function(status) {
+        $scope.header.status = status;
+        $scope.session.user.status = status;
+        
+        $scope.dialog.showStandby();
+        ProfileService.update($scope.session.user).then(function(result) {
+            $scope.dialog.hideStandby();
+            $scope.message.success("更改工作状态成功!");
+            setTimeout(function() {
+                if ($scope.header.status === 'OFFLINE') {
+                    $scope.dialog.confirm({
+                        text: "您是否要退出系统!",
+                        handler: function() {
+                            window.logout();
+                        }
+                    });
+                }
+            }, 1500);
+        }, function() {
+            $scope.dialog.hideStandby();
+            $scope.message.error("更改工作状态失败!");
+        });
+    };
+}])
+.controller('SubHeaderController', function ($scope) {
+    $scope.subheader = {title: "请选择菜单"};
+})
+.directive("ecgHeader", ['$location', function ($location) {
     return {
-      restrict: 'A',
-      replace: true,
-      template: headerTemplate,
-      controller: "HeaderController",
-      link: function ($scope, $element, $attrs) {
-      }
+        restrict: 'A',
+        replace: true,
+        template: headerTemplate,
+        controller: "HeaderController",
+        link: function ($scope, $element, $attrs) {
+        }
     };
-  }])
-  .directive("ecgSubHeader", ['$location', function ($location) {
+}])
+.directive("ecgSubHeader", ['$location', function ($location) {
     return {
-      restrict: 'A',
-      replace: true,
-      template: subheaderTemplate,
-      controller: "SubHeaderController",
-      link: function ($scope, $element, $attrs) {
-      }
+        restrict: 'A',
+        replace: true,
+        template: subheaderTemplate,
+        controller: "SubHeaderController",
+        link: function ($scope, $element, $attrs) {
+        }
     };
-  }]);
+}]);
 
 });
--- web/src/main/webapp/scripts/ie.js
+++ web/src/main/webapp/scripts/ie.js
@@ -9,5 +9,4 @@ document.createElement('at-pagination');
 
 /* ecg custom element definition */
 // monitor module
-document.createElement('ecg-employee-status-report');
 document.createElement('ecg-task-process-report');
