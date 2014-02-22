36fd4edd57d404e70190b31aff68a1cf3b70d8d7
hopesfish <hopesfish@163.com>
2014-02-22 22:02:12 +0800

    revert code

--- web/src/main/webapp/scripts/upassword.js
+++ web/src/main/webapp/scripts/upassword.js
@@ -93,7 +93,7 @@ $(function() {
             }
         }).then(function(res) {
             alert('重置成功');
-            window.location.href = "ulogin.html";
+            window.location.href = "login.html";
         }, function() {
             alert('重置失败!');
         });
